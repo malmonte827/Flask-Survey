493f418ef7fe7480a09da192d39c7317829d7ed9
mike <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-06-19 00:44:25 -0400

    Using flask sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -9,7 +9,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
 
-responses = []
+session_keys = 'responses'
 
 @app.route('/')
 def homepage():
@@ -17,12 +17,20 @@ def homepage():
 
     return render_template('survey-homepage.html', survey = survey)
 
+@app.route('/start', methods=['POST'])
+def start_survey():
+    """Clear session of responses"""
+
+    session[session_keys] = []
+
+    return redirect('/questions/0')
 
 
 @app.route('/questions/<int:question_id>')
 def show_question(question_id):
     """Display Question"""
-
+    responses = session.get(session_keys)
+    
     # Accessing question out of order
     if (len(responses) != question_id):
         flash('Invalid Question ID.')
@@ -41,10 +49,12 @@ def show_question(question_id):
 @app.route('/answer', methods=['POST'])
 def save_answer():
     """ Appends answer to responses redirects to next question"""
-
     # Get answer from form and append them to responses
     choice = request.form['answer']
+
+    responses = session[session_keys]
     responses.append(choice)
+    session[session_keys] = responses
 
     # Answered all questions. Thank them
     if (len(responses) == len(survey.questions)):
